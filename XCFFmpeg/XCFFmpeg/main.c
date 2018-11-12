394d6c7e7f0aa20682e6df453e356831b5c8c3b6
tomxiang <tomxiang@tencent.com>
2018-11-12 20:28:54 +0800

    增加所有文件

---
+++ XCFFmpeg/XCFFmpeg/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  XCFFmpeg
+//
+//  Created by tomxiang on 2018/9/19.
+//  Copyright © 2018年 tomxiang. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
